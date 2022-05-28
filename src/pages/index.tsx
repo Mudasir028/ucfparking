877e1b02f9f277e83d2d7f0ad98e17eafc06c7c4
mudasir028 <imudasir63@gmail.com>
2022-05-29 00:10:13 +0500

    get last row data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,35 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import { Meta } from "../layout/Meta";
-import { Section } from "../layout/Section";
-import { actionCreators } from "../redux";
-import { Charts } from "../template/Charts";
-import { Shell } from "../template/Shell";
-import { Stats } from "../template/Stats";
-import { AppConfig } from "../utils/AppConfig";
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Meta } from '../layout/Meta'
+import { Section } from '../layout/Section'
+import { actionCreators } from '../redux'
+import { Charts } from '../template/Charts'
+import { Shell } from '../template/Shell'
+import { Stats } from '../template/Stats'
+import { AppConfig } from '../utils/AppConfig'
 // import { Welcome } from '../types';
 // import { NextPage } from 'next';
 
 interface Props {
   // graphData: Welcome
-  error: any;
+  error: any
   lastRowData: {
-    total_data_rows: number;
-    last_date_and_time: Date;
-  };
+    total_data_rows: number
+    last_date_and_time: Date
+  }
 }
 
 // const Index: NextPage<Welcome> = (graphData) => {
 const Index = ({ lastRowData, error }: Props) => {
-  const dispatch = useDispatch();
-  const { HandleLastRowData } = bindActionCreators(actionCreators, dispatch);
+  const dispatch = useDispatch()
+  const { HandleLastRowData } = bindActionCreators(actionCreators, dispatch)
 
   useEffect(() => {
     if (!error) {
-      HandleLastRowData(lastRowData);
+      HandleLastRowData(lastRowData)
     }
-  }, []);
+  }, [])
   return (
     <>
       <Meta title={AppConfig.title} description={AppConfig.description} />
@@ -43,29 +43,29 @@ const Index = ({ lastRowData, error }: Props) => {
         </Section>
       </Shell>
     </>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   try {
     // const res = await fetch("https://api.ucfparking.com");
     // const graphData = await res.json();
-    const res1 = await fetch("https://api.ucfparking.com/stats");
-    const lastRowData = await res1.json();
+    const res = await fetch(`https://api.ucfparking.com/stats`)
+    const lastRowData = await res.json()
 
     return {
       props: {
         // graphData,
         lastRowData
       }
-    };
+    }
   } catch (error) {
     return {
       props: {
         error
       }
-    };
+    }
   }
-};
+}
